ca3ccd623d03ade0f96bb597900f7a846540be2b
yuzexia <327932801@qq.com>
2017-03-29 16:17:34 +0800

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App';
+
+Vue.use(VueRouter);
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
-})
+  components: {App}
+});
