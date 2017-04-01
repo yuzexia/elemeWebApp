8bcca6f27a4a728b23e6891adcd747f77ba2efae
yuzexia <327932801@qq.com>
2017-04-01 14:38:25 +0800

    goods part

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,51 @@
 <template>
-  <div>
-    I am goods
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  const ERR_OK = 0;
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: []
+      };
+    },
+    created() {
+      this.$http.get('/api/goods').then((res) => {
+        console.log(res);
+        res = res.body;
+        if (res.errno === ERR_OK) {
+          this.goods = res.data;
+          console.log(this.goods);
+        };
+      });
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      /*一个兼容性写法，不写Android手机上会有问题*/
+      width: 80px
+      background: #f3f5f7
+    .foods-wrapper
+      flex: 1
 
 </style>
